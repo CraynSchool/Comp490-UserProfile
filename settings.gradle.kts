eb60954594cf9411995f7ac4055a9acd10d739b6
Crayn <crayn.sanchez.college@gmail.com>
2023-10-19 15:01:01 -0700

    version 1

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Comp 490 - User Profile"
+include(":app")
